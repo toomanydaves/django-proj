35c1ca1bb3f6aaaed7b9a8c63cdf3b5cedeaedf5
Sean Jacke <sean.jacke@learnship.com>
2017-08-27 22:25:11 +0200

    fixes typo bug preventing user creation

--- django_proj/toomanydaves_auth/models.py
+++ django_proj/toomanydaves_auth/models.py
@@ -35,7 +35,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, username, email, first_name, last_name, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fileds.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_superuser', True)
 
         
         if extra_fields.get('is_staff') is not True:
