28279ae12e5f155edb45d05fff7517a613f6a085
Sean Jacke <sean.jacke@learnship.com>
2017-09-06 23:44:04 +0200

    adds Project model to Portfolio app https://trello.com/c/4caVey5f

--- django_proj/portfolio/models.py
+++ django_proj/portfolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Project(models.Model):
+    url = Models.UrlField(unique=True)
+    name = Models.CharField()
+    description = Models.TextField()
+    UPCOMING = 'UPCOMING'
+    ACTIVE = 'ACTIVE'
+    ARCHIVED = 'ARCHIVED'
+    status = Models.CharField(
+        choices=(
+            (UPCOMING, _('upcoming')), 
+            (ACTIVE, _('active')),
+            (ARCHIVED, _('archived')),
+        ),
+        default=UPCOMING,
+    )
+    INTERNAL = 'INTERNAL'
+    EXTERNAL = 'EXTERNAL'
+    origin = Models.CharField(
+        choices=(
+            (INTERNAL, _('internal')),
+            (EXTERNAL, _('external')),
+        ),
+        default=INITIATIVE,
+    )
