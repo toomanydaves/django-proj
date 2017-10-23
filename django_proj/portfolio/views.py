e1d037262b4eca602d4cfb109c97d363ec43da3d
Sean Jacke <sean.jacke@learnship.com>
2017-10-23 16:41:36 +0200

    adds a little pop to join text

--- django_proj/portfolio/views.py
+++ django_proj/portfolio/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Project
+from .models import Project, Tag
 
 def home(request):
     project_list = Project.objects.all()
-    return render(request, 'portfolio/projects.html', {'project_list': project_list})
+    tag_list = Tag.objects.all()
+    return render(request, 'portfolio/projects.html', {
+        'project_list': project_list,
+        'tag_list': tag_list,
+    })
 
 def project(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
