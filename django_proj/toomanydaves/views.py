159cc71614de917902800bf78790a3c222732c86
Sean Jacke <sean.jacke@learnship.com>
2017-12-12 10:19:03 +0100

    UX for home page

--- django_proj/toomanydaves/views.py
+++ django_proj/toomanydaves/views.py
@@ -1,12 +1,10 @@
+from blog.models import Post
 from django.contrib.auth import logout
 from django.shortcuts import render
 from portfolio.models import Project
 
 def home(request):
-    project_list = Project.objects.order_by('?')[:4]
     seen_home = True
-
-    print(project_list)
 
     if request.session.get('seen_home', False):
         request.session['seen_home'] = True
@@ -14,7 +12,8 @@ def home(request):
 
     return render(request, 'toomanydaves/home.html', {
         'seen_home': seen_home,
-        'project_list': project_list,
+        'project_list': Project.objects.order_by('?')[:4],
+        'post_list': Post.objects.filter(status='PUBLISHED').order_by('-published_at')[:4],
     })
 
 def backstory(request):
