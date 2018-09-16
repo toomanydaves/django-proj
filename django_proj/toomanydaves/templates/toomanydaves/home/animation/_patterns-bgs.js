0c925c173014311b282f3ce05db3291e84030343
Sean Jacke <sean.jacke@learnship.com>
2018-09-16 21:10:36 +0200

    fixes js loading

---
+++ django_proj/toomanydaves/templates/toomanydaves/home/animation/_patterns-bgs.js
@@ -0,0 +1,22 @@
+var patternBgs = [
+    // Open Eyes
+    'https://i.pinimg.com/564x/2f/a8/fb/2fa8fb853f50cf09f546058c9fb09f7a.jpg',
+    // Moths
+    'https://i.pinimg.com/564x/52/18/ea/5218eaa101be2e8c8c60671cd431d553.jpg',
+    // Pink and blue leopard
+    'https://i.pinimg.com/564x/fa/7a/1b/fa7a1bcad570ccdb773188bcc47828d1.jpg',
+    // Tropical block-print leaves
+    'https://i.pinimg.com/564x/11/82/b7/1182b78b25856850202557d547227208.jpg',
+    // City of Cherries
+    'https://i.pinimg.com/564x/7b/44/7d/7b447dff4fd0daf6783fa69c6ea399f4.jpg',
+    // Aqua Diamond tiles
+    'https://i.pinimg.com/564x/c0/a9/d7/c0a9d73d526d03c4478907a5a418930d.jpg',
+    // Overlapping Leaves
+    'https://i.pinimg.com/564x/ab/67/e3/ab67e3911aa60cf6c14e4f3d2ad628c8.jpg',
+    // Albers-like squares
+    'https://i.pinimg.com/564x/d6/e4/12/d6e41292172d45dd058c5051d1d9776e.jpg',
+    // Kaleidoscope 
+    'https://i.pinimg.com/564x/92/4f/4d/924f4d0df85c94964ae6f0f65953a5e1.jpg',
+    // Ellsworth Kelly
+    'https://i.pinimg.com/564x/ff/ed/34/ffed3489819c2e36113d8ca75f7bb4ff.jpg'
+];
